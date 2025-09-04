44ec68074248d1a0f0e5bc6015a94aa79b2a3a73
Banuka <banukadeseram12@gmail.com>
2025-09-04 23:10:24 +0530

    finished

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from mangum import Mangum
 # Add project directory to path
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myprojects.settings")
 
 from django.core.asgi import get_asgi_application
 application = get_asgi_application()
